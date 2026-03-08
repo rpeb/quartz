b64ec6b6074cd0fcd99ee0dcecaa08d9a5890aca
Prakash Dubey <prakashdubey1999@gmail.com>
2026-03-08 14:44:47 +0530

    deploying on a new website

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "www.prakashdubey.online",
+    baseUrl: "log.quietoverflow.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
